0bdebf7353cc728ade4881f14f0877ff843fbbba
mkhoidzvl13 <112261602+mkhoidzvl13@users.noreply.github.com>
2024-01-28 22:32:35 +0700

    Update mkhoi.lua

--- mkhoi.lua
+++ mkhoi.lua
@@ -4,7 +4,7 @@ local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.
 --------------------------------------------------------------------------------------------------------------------------------------------
 local Window = Fluent:CreateWindow({
     Title = "minhkhoi Hub",
-    SubTitle = "https://discord.gg/n4dbVKNJ",
+    SubTitle = "anh iu bich ngoc",
     TabWidth = 160,
     Size = UDim2.fromOffset(450, 300),
     Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
