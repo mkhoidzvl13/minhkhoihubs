3c69658fa1ab10aff0da5927576b9f9f58edc206
mkhoidzvl13 <112261602+mkhoidzvl13@users.noreply.github.com>
2024-01-29 22:46:33 +0700

    Update mkhoi.lua

--- mkhoi.lua
+++ mkhoi.lua
@@ -6288,4 +6288,11 @@ Tabs.info:AddButton({
  setclipboard("https://www.facebook.com/61551675837476")
   DiscordLib:Notification("Thông Báo" , "Đã Sao Chép Link Facebook" , 10 ,11816162894 )
 	end
+Tabs.info:AddButton({
+	Title = "click để lấy link discord",
+	Description = "",
+	Callback = function()
+ setclipboard("https://discord.gg/GYvqFUEr")
+  DiscordLib:Notification("Thông Báo" , "Đã Sao Chép Link discord" , 10 ,11816162894 )
+	end
 })
