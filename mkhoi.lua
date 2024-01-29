499dee2e5a0c2231f1f719676329f84d2c1a47f8
mkhoidzvl13 <112261602+mkhoidzvl13@users.noreply.github.com>
2024-01-29 17:34:55 +0700

    Update mkhoi.lua

--- mkhoi.lua
+++ mkhoi.lua
@@ -2309,7 +2309,7 @@ ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 ImageButton.Parent = ScreenGui
-ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+
 ImageButton.Position = UDim2.new(0.10615778, 0, 0.16217947, 0)
 ImageButton.Size = UDim2.new(0.0627121851, 0, 0.107579626, 0)
 ImageButton.Image = "rbxassetid:// 16152222934"
