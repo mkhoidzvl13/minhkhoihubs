cf118c0a4b8c402862d1cefd182d03663cff3bdd
mkhoidzvl13 <112261602+mkhoidzvl13@users.noreply.github.com>
2024-01-29 22:34:38 +0700

    Update mkhoi.lua

--- mkhoi.lua
+++ mkhoi.lua
@@ -22,6 +22,7 @@ local Tabs = {
     Race = Window:AddTab({ Title = "Race V4", Icon = "chevrons-right" }),
     Shop = Window:AddTab({ Title = "Shop", Icon = "shopping-cart" }),
 	Misc = Window:AddTab({ Title = "Misc", Icon = "list-plus" }),
+	facebook = Window:AddTab({ Title = "facebook", Icon = "facebook" }),
 }
 local Options = Fluent.Options
 
